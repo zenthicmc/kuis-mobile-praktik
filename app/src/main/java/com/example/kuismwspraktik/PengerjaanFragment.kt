126ac7e758035bac078b4d685d0d7bc0ea1e2a82
Jagad R R <jagadraya388@gmail.com>
2022-11-04 10:00:51 +0700

    Update PengerjaanFragment.kt

--- app/src/main/java/com/example/kuismwspraktik/PengerjaanFragment.kt
+++ app/src/main/java/com/example/kuismwspraktik/PengerjaanFragment.kt
@@ -1,7 +1,6 @@
 package com.example.kuismwspraktik
 
 import android.os.Bundle
-import android.provider.ContactsContract.Data
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
